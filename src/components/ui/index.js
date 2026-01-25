c9a157b4d18c11766d0113ae451df3769697c3f4
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-01-25 12:03:28 +0000

    add index.js to export UI components for centralized access

---
+++ src/components/ui/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from "./Button";
+export { default as NodeField } from "./NodeField";
+export { default as Select } from "./Select";
+export { default as TextArea } from "./TextArea";
+export { default as TextInput } from "./TextInput";
