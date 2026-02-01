efe65f4892866805c8a28e736687d11531fb9e73
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-02-01 16:15:55 +0000

    refactor App component to include Toolbar and Canvas for enhanced layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
-  return <div>PipelineForge</div>;
-}
+import { Toolbar } from "./components/toolbar";
+import { Canvas } from "./components/canvas";
 
-export default App;
+export default function App() {
+  return (
+    <div className="w-full h-dvh relative p-2">
+      <div className="size-full relative flex flex-col border-1.5 border-gray-200 rounded-lg overflow-hidden">
+        <Toolbar />
+        <Canvas />
+      </div>
+    </div>
+  );
+}
