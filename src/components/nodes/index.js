d4921ded611b1b15db6a784d422300e23c49e038
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2026-02-06 05:33:22 +0000

    add CodeNode to NODE_TYPES for code node support

--- src/components/nodes/index.js
+++ src/components/nodes/index.js
@@ -1,7 +1,9 @@
 import InputNode from "./InputNode";
 import TextNode from "./TextNode";
+import CodeNode from "./CodeNode";
 
 export const NODE_TYPES = {
   input: InputNode,
   text: TextNode,
+  code: CodeNode,
 };
