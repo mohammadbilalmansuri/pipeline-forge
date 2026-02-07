555283e122e178c661ccc6fba6dfa9f964ffa4b2
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-02-07 02:39:10 +0000

    add DelayNode to NODE_TYPES for delay node support

--- src/components/nodes/index.js
+++ src/components/nodes/index.js
@@ -1,9 +1,11 @@
 import InputNode from "./InputNode";
 import TextNode from "./TextNode";
 import CodeNode from "./CodeNode";
+import DelayNode from "./DelayNode";
 
 export const NODE_TYPES = {
   input: InputNode,
   text: TextNode,
   code: CodeNode,
+  delay: DelayNode,
 };
