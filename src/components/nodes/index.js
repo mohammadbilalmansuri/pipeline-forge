6d33b1cacd5caa686d085c0e03b3691c48688e9b
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-02-08 02:20:41 +0000

    add NoteNode to NODE_TYPES for note support

--- src/components/nodes/index.js
+++ src/components/nodes/index.js
@@ -2,10 +2,12 @@ import InputNode from "./InputNode";
 import TextNode from "./TextNode";
 import CodeNode from "./CodeNode";
 import DelayNode from "./DelayNode";
+import NoteNode from "./NoteNode";
 
 export const NODE_TYPES = {
   input: InputNode,
   text: TextNode,
   code: CodeNode,
   delay: DelayNode,
+  note: NoteNode,
 };
