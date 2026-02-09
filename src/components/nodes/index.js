eca6a32f65f0447304598f8b363ba25343c9c7af
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2026-02-09 02:01:25 +0000

    add OutputNode to NODE_TYPES for output support

--- src/components/nodes/index.js
+++ src/components/nodes/index.js
@@ -3,6 +3,7 @@ import TextNode from "./TextNode";
 import CodeNode from "./CodeNode";
 import DelayNode from "./DelayNode";
 import NoteNode from "./NoteNode";
+import OutputNode from "./OutputNode";
 
 export const NODE_TYPES = {
   input: InputNode,
@@ -10,4 +11,5 @@ export const NODE_TYPES = {
   code: CodeNode,
   delay: DelayNode,
   note: NoteNode,
+  output: OutputNode,
 };
