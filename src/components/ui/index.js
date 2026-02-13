9308c8e12370f8e777d1baa5974e9660204cb6a7
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2026-02-13 01:55:12 +0000

    add KeyValueEditor export to UI components index

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -3,3 +3,4 @@ export { default as NodeField } from "./NodeField";
 export { default as Select } from "./Select";
 export { default as TextArea } from "./TextArea";
 export { default as TextInput } from "./TextInput";
+export { default as KeyValueEditor } from "./KeyValueEditor";
